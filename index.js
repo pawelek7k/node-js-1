6535659d89e7781da6dd3dd3ff84fede191396fb
pawelek <k.pawelek@vp.pl>
2024-06-12 11:06:24 +0200

    add commander

--- index.js
+++ index.js
@@ -5,6 +5,18 @@ const {
   addContact,
 } = require("./contacts.js");
 const yargs = require("yargs");
+const { Command } = require("commander");
+const program = new Command();
+program
+  .option("-a, --action <type>", "choose action")
+  .option("-i, --id <type>", "user id")
+  .option("-n, --name <type>", "user name")
+  .option("-e, --email <type>", "user email")
+  .option("-p, --phone <type>", "user phone");
+
+program.parse(process.argv);
+
+const argv = program.opts();
 
 async function invokeAction({ action, id, name, email, phone }) {
   switch (action) {
@@ -49,5 +61,4 @@ async function invokeAction({ action, id, name, email, phone }) {
   }
 }
 
-const action = argv._[0];
-invokeAction({ action, ...argv });
+invokeAction(argv);
